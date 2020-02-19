a3c6f7aeb605f7306023762137761bf500518279
Robert LaDuca <rladuca@gmail.com>
2020-02-18 23:16:53 -0800

    Test

---
+++ AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+using System.Reflection;
+
+// Now we can set the wildcard here
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.*")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+
+[assembly:ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
